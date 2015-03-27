83e36652b1ba43a599b3550f18ae683d53dd482f
tkaviya <tsungai.kaviya@gmail.com>
2015-03-28 00:21:03 +0200

    persistence

--- sym_web/src/main/java/net/blaklizt/symbiosis/sym_web/controllers/DefaultController.java
+++ sym_web/src/main/java/net/blaklizt/symbiosis/sym_web/controllers/DefaultController.java
@@ -15,9 +15,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 
 import org.json.JSONObject;
 
