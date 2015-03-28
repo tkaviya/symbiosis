bd352762bf3b0bcf3a8bc14dfbcc5a2d65374d39
tkaviya <chess02>
2015-03-28 11:18:37 +0200

    user attribute

--- sym_persistence/src/main/java/net/blaklizt/symbiosis/sym_persistence/UserAttribute.java
+++ sym_persistence/src/main/java/net/blaklizt/symbiosis/sym_persistence/UserAttribute.java
@@ -13,13 +13,22 @@ import javax.persistence.*;
 public class UserAttribute {
 
 	private Long authUserID;
-
+	private String firstName;
+	private String lastName;
+	private String email;
+	private String msisdn;
+	private Integer countryID;
 
 	@javax.persistence.OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
 	@javax.persistence.JoinTable(name="SymbiosisUser",
 			   joinColumns = {@javax.persistence.JoinColumn(name="SymbiosisUserID", referencedColumnName="SymbiosisUserID")},
 		inverseJoinColumns = {@javax.persistence.JoinColumn(name="SymbiosisUserID", referencedColumnName="SymbiosisUserID")})
 	SymbiosisUser symbiosisUser;
+
+//	@javax.persistence.ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+//	@javax.persistence.JoinTable(name="County",
+//			   joinColumns = {@javax.persistence.JoinColumn(name="CountryID", referencedColumnName="CountryID")},
+//	Country country;
 
 	@Column(name = "AuthUserID", nullable = false, insertable = true, updatable = true)
 	@Id
@@ -29,6 +38,56 @@ public class UserAttribute {
 
 	public void setAuthUserID(Long authUserID) {
 		this.authUserID = authUserID;
+	}
+
+	@Column(name = "FirstName", nullable = false, insertable = true, updatable = true)
+	@Basic
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	@Column(name = "LastName", nullable = false, insertable = true, updatable = true)
+	@Basic
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	@Column(name = "Email", nullable = false, insertable = true, updatable = true)
+	@Basic
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	@Column(name = "Msisdn", nullable = false, insertable = true, updatable = true)
+	@Basic
+	public String getMsisdn() {
+		return msisdn;
+	}
+
+	public void setMsisdn(String msisdn) {
+		this.msisdn = msisdn;
+	}
+
+	@Column(name = "CountryID", nullable = false, insertable = true, updatable = true)
+	@Basic
+	public Integer getCountryID() {
+		return countryID;
+	}
+
+	public void setCountryID(Integer countryID) {
+		this.countryID = countryID;
 	}
 
 	public SymbiosisUser getSymbiosisUser() {
