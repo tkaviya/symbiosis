a3d46c43536cdcf4603cb6b39faf648238d65390
tkaviya-linux <tsungai.kaviya@gmail.com>
2015-09-15 06:18:28 +0200

    db up

--- sym_persistence/src/main/java/net/blaklizt/symbiosis/sym_persistence/dao/SymbiosisAuthUserDao.java
+++ sym_persistence/src/main/java/net/blaklizt/symbiosis/sym_persistence/dao/SymbiosisAuthUserDao.java
@@ -1,10 +1,13 @@
 package net.blaklizt.symbiosis.sym_persistence.dao;
 
 import net.blaklizt.symbiosis.sym_persistence.SymbiosisUser;
+import org.springframework.stereotype.Repository;
 
 /**
  * Created by tsungai.kaviya on 2015-08-24.
  */
+
+@Repository("symbiosisAuthUserDao")
 public interface SymbiosisAuthUserDao {
 
     SymbiosisUser findByUserIDAndChannel(Long userID, int channelID);
--- sym_persistence/src/main/java/net/blaklizt/symbiosis/sym_persistence/dao/SymbiosisUserDao.java
+++ sym_persistence/src/main/java/net/blaklizt/symbiosis/sym_persistence/dao/SymbiosisUserDao.java
@@ -1,10 +1,12 @@
 package net.blaklizt.symbiosis.sym_persistence.dao;
 
 import net.blaklizt.symbiosis.sym_persistence.SymbiosisUser;
+import org.springframework.stereotype.Repository;
 
 /**
  * Created by tsungai.kaviya on 2015-08-24.
  */
+@Repository
 public interface SymbiosisUserDao {
     SymbiosisUser findByUsername(String username);
 
